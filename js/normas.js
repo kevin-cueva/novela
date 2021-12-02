4e107f3ea92a8b9b64b8586e5de7c09a58fb391c
Kellypadi <90870209+Kellypadi@users.noreply.github.com>
2021-12-02 13:21:49 -0500

    Update normas.js

--- js/normas.js
+++ js/normas.js
@@ -1,11 +1,10 @@
 monogatari.script ({
 
-          'Start_Normas': [
+         'Start_Normas': [
+	    
 	        'show background global at left with fadeIn', 
 	   
 	        'show character t normal at left with fadeIn', 
-		  
-		 
 	   
 	    't: Estimado Aprendiz,',
 	    
@@ -17,11 +16,11 @@ monogatari.script ({
 	        'show character t feliz at center with fadeIn',
 	 
             't: A continuación vamos a evaluar sus conocimientos en,',
-            't: Normativa comercial, contable, financiera vigente y políticas contables.',
-		  
-		  'hide character t normal with fadeOut', 
-		  
-		   {
+            't: Normativa comercial, contable y financiera vigente y políticas contables.',
+	 
+                'hide character t normal with fadeOut', 
+	    
+	    {
 	       'Choice': {
 	         
 	           'Dialog': 't: Querido Aprendiz, constituida su empresa seleccione el paso a seguir',
@@ -30,7 +29,6 @@ monogatari.script ({
 	           'p1': {
 	             'Text': 'Definir marco conceptual aplicable',
 	             'Do': 'jump Definirmarcoconceptualaplicable'
-			   
 	           },
 	         
 	           'p2': {
@@ -49,12 +47,13 @@ monogatari.script ({
 	           },
 	
 	       } 
-	    },   
-		  
-		'end',
-	           ], 	
-
+	    },    
+	    
+	            'end',
+	  
+	    
 	   'Definirmarcoconceptualaplicable': [
+		   
 		   'show background auditoria at left with fadeIn', 
 	   
 	           'show character t brava at center with fadeIn',
@@ -69,11 +68,13 @@ monogatari.script ({
                't: el paso a seguir  es definir el marco conceptual contable y financiero aplicable a su empresa,',
                't: de acuerdo a actividad comercial, necesidades de la empresa y la normativa vigente',
 		   
+		 
 	       'end',
 	           ],
 
 	  
 	  'Identificarlaestructuracontable': [
+		  
 	           'show background estructura at left with fadeIn',
 		  
 	           'show character t brava at center with fadeIn',
@@ -88,20 +89,35 @@ monogatari.script ({
                't: el paso a seguir es identificar la estructura contable aplicable a su empresa.',
                't: De acuerdo al proyecto formativo, usted debe tener como referencia el marco normativo',
                't: del grupo 2 normas NIIF para PYMES, por lo cual es necesario Identificar si su empresa cumple con los', 
-               't: requisitos para ser catalogadas como pequeña y mediana empresa',	  
+               't: requisitos para ser catalogadas como pequeña y mediana empresa',
+		  
 		  
 	       'end',
 	           ], 
 	  
 	  'Conocernormativavigente': [
+		  
 		  'show background internacional at left with fadeIn', 
 	   
 	          'show character t feliz at center with fadeIn',
+	     
+	      't: Querido Aprendiz',
+		  
+		   
+               't: Las Normas Internacionales de Información Financiera constituyen el conjunto de normas contables,',
+               't: con mayor aceptación a nivel mundial, su objetivo es regular la preparación de información transparente y objetiva.',
+               't: estas normas pretenden generar un lenguaje universal y comprensible para las empresas nacionales e internacionales',
+
+	      'show background decisiones at left with fadeIn',
+		  
+               't: Una vez constituida su empresa el  paso a seguir es conocer la normativa comercial, contable y',
+               't: financiera vigente en Colombia específicamente, todo lo referente a las NIIF', 
+               't: su objetivo e identificar el organismo emisor',
 		  
 		   {
 	       'Choice': {
 	         
-	           'Dialog': 't: Querido Aprendiz',
+	           'Dialog': 't: Querido Aprendiz, presiona los siguientes botones para profundizar un poco más',
 		  
 		   'p5': {
 	             'Text': 'Objetivo de las NIIF',
@@ -117,25 +133,16 @@ monogatari.script ({
 	             'Text': 'Organismo emisor de las NIIF',
 	             'Do': 'jump OrganismoemisordelasNIIF'
 	           },
-			   			   
+			   
+			   
 	       } 
 	    },
-	         
-               't: Las Normas Internacionales de Información Financiera constituyen el conjunto de normas contables,',
-               't: con mayor aceptación a nivel mundial, su objetivo es regular la preparación de información transparente y objetiva.',
-               't: estas normas pretenden generar un lenguaje universal y comprensible para las empresas nacionales e internacionales',
-
-	      'show background decisiones at left with fadeIn',
-		  
-               't: Una vez constituida su empresa el  paso a seguir es conocer la normativa comercial, contable y',
-               't: financiera vigente en Colombia específicamente, todo lo referente a las NIIF', 
-               't: su objetivo e identificar el organismo emisor', 	  
-	       't:  Aprendiz, presiona los siguientes botones para profundizar un poco más',
 		  
 	       'end',
 	           ], 	   
 	
 	  'Crearpoliticascontables':[
+		  
 		   'show background empresarial at left with fadeIn', 
 	   
 		   'show character t shock at center with fadeIn',
@@ -150,29 +157,35 @@ monogatari.script ({
 	       't: el paso a seguir  es conocer las normas a tener en cuenta para la selección y',
 	       't: aplicación de las políticas contables  conforme a los lineamientos de las NIIF para PYMES', 
 
+	    
 		'end',
 	            ],
 		  
 		  
 	  'ObjetivodelasNIIF':[
+		  
 		  'show background internacional at left with fadeIn',
 		  'show character t normal at center with fadeIn',
 		  
 	       't: Las NIIF tiene como objetivo, regular la preparación de información transparente y objetiva.',
 	       't: Estas normas pretenden generar un lenguaje universal  comprensible, para las empresas nacionales e internacionales',
 		 
-		  'jump Conocernormativavigente'
-		 
-	            
+		  'jump Conocernormativavigente', 
+		  
+	       'end',
+	            ],
 		  
           'CualidadesdelainformaciónbajoNIIF':[
 		  'show background internacional at left with fadeIn',
 		   'show character t normal at center with fadeIn',
+		  	  
+	       't: En el marco de la normatividad internacional, se estableció 9 principios o cualidades básicas para la', 
+	       't: información financiera presentada, las cuales se han dividido en dos secciones:',
 		  
 		   {
 	       'Choice': {
 	         
-	           'Dialog': 't: Querido Aprendiz, para conocer las cualidades presiona los siguientes botones',
+	           'Dialog': 't: Querido Aprendiz, presiona los siguientes botones donde se mencionan las cualidades ',
 		  
 		   'p8': {
 	             'Text': 'Cualidades primarias',
@@ -186,10 +199,7 @@ monogatari.script ({
 			    
 	       } 
 	    },
-		  	  
-	       't: En el marco de la normatividad internacional, se estableció 9 principios o cualidades básicas para la', 
-	       't: información financiera presentada, las cuales se han dividido en dos secciones:',
-		   
+		  
 	       'end',
 	            ],
 			  
@@ -200,6 +210,8 @@ monogatari.script ({
 	       't: Querido Aprendiz', 
 	       't: La Junta de Normas Internacionales de Contabilidad, en ingles International Accounting Standards Board IASB,', 
 	       't: es el organismo que emite dicho cuerpo de normas desde el año 2001',
+		  
+		  'jump Start_Normas', 
 		
 	       'end',
 	            ],
@@ -233,5 +245,7 @@ monogatari.script ({
 	  
 	});
 	
+
+
 
 
